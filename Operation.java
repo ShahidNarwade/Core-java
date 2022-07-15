44f33b9089aae2cd652e32decc9afa725426c748
ShahidNarwade <94898655+ShahidNarwade@users.noreply.github.com>
2022-07-15 18:01:41 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,28 @@
+
+public class Operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 10;
+
+		int c = a + b;
+
+		System.out.println("Addition Of Two Number Is :" + c);
+
+		int d = a - b;
+		System.out.println("Substraction Of Two Number Is :" + d);
+
+		int e = a * b;
+		System.out.println("Multiplication Of Two Number Is :" + e);
+		
+		int f = a/b ;
+		System.out.println("Divide Of Number is :"+f);
+		
+		int g = a%b ;
+		System.out.println("moduls of number is"+g);
+
+	}
+
+}
